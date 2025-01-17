1b740db9e4c90a42464e08b322a82d22265bdf5c
Praveen Kumar Pandey <praveenpandeypandey034@gmail.com>
2025-01-17 08:57:54 +0530

    this is initial commit

---
+++ cript.js
@@ -0,0 +1,12 @@
+document.getElementById('send-money').addEventListener('click', () => {
+    alert('Send Money feature coming soon!');
+  });
+  
+  document.getElementById('receive-money').addEventListener('click', () => {
+    alert('Receive Money feature coming soon!');
+  });
+  
+  document.getElementById('bank-link').addEventListener('click', () => {
+    alert('Link Bank feature coming soon!');
+  });
+  
